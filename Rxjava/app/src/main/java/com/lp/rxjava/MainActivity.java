756cf370d540beed212110d7efda55dd589717ca
lipin <1079107009@qq.com>
2017-06-23 09:14:23 +0800

    rxjava

---
+++ Rxjava/app/src/main/java/com/lp/rxjava/MainActivity.java
@@ -0,0 +1,53 @@
+package com.lp.rxjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //创建一个上游 Observable
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                e.onNext(0);
+                e.onNext(1);
+                e.onNext(2);
+                e.onComplete();
+            }
+        }).subscribe(new Observer<Integer>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "subscribe");
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, "" + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "complete");
+            }
+
+        });
+    }
+}
